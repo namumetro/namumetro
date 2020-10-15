b34dc0d0037e8a8fdf92cd7b9bef8f616bdea5ed
namumetro <71622706+namumetro@users.noreply.github.com>
2020-10-15 11:54:33 +0900

    Update signCheck.php

--- signCheck.php
+++ signCheck.php
@@ -7,12 +7,14 @@ if (mysqli_connect_error()) {
 
 extract($_POST);
 
+$encrypted_pass = hash("sha256", $wser_pass);
+
 echo $user_id. '<br />';
 echo $user_pass. '<br />';
 echo $user_pass2. '<br />';
 echo $user_email. '<br />';
 
-$q = "INSERT INTO ap_member ( id, pw, email ) VALUES ( '$user_id', '$user_pass', '$user_email' )";
+$q = "INSERT INTO ap_member ( id, pw, email ) VALUES ( '$user_id', '$encryped_pass', '$user_email' )";
 
 $mysqli->query( $q);
 

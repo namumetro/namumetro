7b73f9b712badaa5b03b21b61fcdcd04b8e7c98a
namumetro <71622706+namumetro@users.noreply.github.com>
2020-10-15 10:16:01 +0900

    Delete rkskek.sql

--- rkskek.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `student` (
-    `id`  tinyint NOT NULL ,
-    `name`  char(4) NOT NULL ,
-    `sex`  enum('남자','여자') NOT NULL ,
-    `address`  varchar(50) NOT NULL ,
-    `birthday`  datetime NOT NULL ,
-    PRIMARY KEY (`id`)
-);
